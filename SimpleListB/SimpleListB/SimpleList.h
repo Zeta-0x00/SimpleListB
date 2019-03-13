8ce21c483019ff946c9f39605d16fbfedcf07196
Johel Mora <Johel Mora@LAPTOP-NFUCADGT>
2019-03-12 22:24:35 -0600

    Push_Back completo Push_Front en proceso Insert en proceso

--- SimpleListB/SimpleListB/SimpleList.h
+++ SimpleListB/SimpleListB/SimpleList.h
@@ -1,8 +1,13 @@
 #pragma once
+#ifndef SimpleList_h
+#define SimpleList_h
+
 #include <iostream>
 #include <string>
+
 using std::string;
 using std::cout;
+
 template <class T>
 class List {
 private:
@@ -30,12 +35,12 @@ public:
 	void push_back(T x);
 	void insertar(T x, int pos);
 	void insertarR(T x, int pos);
-	bool remove(int pos, T& x);	
+	bool remove(int pos, T& x);
 	bool pop(T& x);
 	bool pop_back(T& x);
 	bool get(int pos, T& element);
-	bool get_front(T& element);		
-	bool get_back(T& element);		
+	bool get_front(T& element);
+	bool get_back(T& element);
 	void print();
 	~List();
 
@@ -135,6 +140,7 @@ bool List<T>::pop(T & x)
 		x = p->elemento;
 		primero = p->siguiente;
 		delete p;
+		return true;
 	}
 }
 
@@ -153,6 +159,7 @@ bool List<T>::pop_back(T & x)
 		c->siguiente = p->siguiente;
 		x = p->elemento;
 		delete p;
+		return true;
 	}
 }
 
@@ -167,7 +174,7 @@ bool List<T>::get(int pos, T & element)
 			p = p->siguiente;
 			pos--;
 		}
-		elemen = p->elemento;
+		element = p->elemento;
 		return true;
 	}
 	return false;
@@ -176,7 +183,7 @@ bool List<T>::get(int pos, T & element)
 template<class T>
 bool List<T>::get_front(T & element)
 {
-	if(!primero)
+	if (!primero)
 		return false;
 	else
 	{
@@ -188,7 +195,7 @@ bool List<T>::get_front(T & element)
 template<class T>
 bool List<T>::get_back(T & element)
 {
-	if(!primero)
+	if (!primero)
 		return false;
 	else
 	{
@@ -233,3 +240,4 @@ List<T>::~List() {
 		primero = p;
 	}
 }
+#endif // !SimpleList_h
--- SimpleListB/SimpleListB/SimpleListB.h
+++ SimpleListB/SimpleListB/SimpleListB.h
@@ -1,23 +1,179 @@
 #pragma once
+#ifndef SimpleListB_h
+#define SimpleListB_h
+#include<iostream>
+using namespace std;
+
+template<class T, int N = 10>
 class SimpleListB
-{private:
-	template <class T>
+{
 	struct Nodo
 	{
-
+		//region Properties
+		bool Full;	//Indicator if the array is fully
+		T element[N];	//Array to N elements
+		struct Nodo* Next; //Pointer to next node
+		//end region
+		//region Contructors
+		Nodo() : Full{ false }, Next{ nullptr }{}
+		Nodo(struct Nodo* X) : Full { false }, Next{ X }{}
+		//end region
 	};
+	typedef struct Nodo* Link;
+	//region Properties
+	Link Head;
+	int Size;
+	int Tam;
+	string ListName;
+	//end region
 public:
-	//SimpleListB();
-	//~SimpleListB();
+	//region Declarations
+	SimpleListB(string);
+	~SimpleListB();
+	int length() { return this->Size; }
+	void push_front(T x);
+	void push_back(T x);
+	void insert(T x, int pos);
+	bool remove(int pos, T& x);
+	bool pop(T& x);
+	bool pop_back(T& x);
+	bool get(int pos, T& x);
+	bool get_front(T& x);
+	bool get_back(T& x);
+	void print();
+	//end region
 };
 
-
-
-/*SimpleListB::SimpleListB()
+//region Definitions
+template<class T, int N>
+SimpleListB<T, N>::SimpleListB(string n)
 {
+	this->Head = NULL;
+	this->ListName = n;
+	this->Size = 0;
 }
 
-
-SimpleListB::~SimpleListB()
+template<class T, int N>
+SimpleListB<T, N>::~SimpleListB()
 {
-}*/
+	Link p;
+	while (Head)
+	{
+		p = Head->Next;
+		delete Head;
+		Head = p;
+	}
+}
+template<class T, int N>
+void SimpleListB<T, N>::push_front(T x)
+{
+	Link p = Head;
+	if (!Head) {
+		Head = new Nodo();p = Head;
+	}
+	if(Tam==N) {
+		Head = new Nodo(p);
+		p->Full = true;
+		Size++;
+		Tam = 0;
+	}
+	if (Tam==0){
+	for (int i = 0; i < N; i++) { p->element[i] = NULL; }
+	}cout << Head->element[0] << endl;
+	if (Head->element[0] != NULL) {
+		int i = N- 1;
+		while (i > -1) {
+			Head->element[i] = Head->element[i-1];
+			i--;
+		}
+	}Tam++;
+	Head->element[0] = x;
+}
+template<class T, int N>
+void SimpleListB<T, N>::push_back(T x)
+{
+	Link p=Head;
+	if (!Head) { Head = new Nodo(); p = Head; }
+	while (p->Next != nullptr)
+		p = p->Next;
+	if (Tam < N) {
+		if (Tam == 0) {
+			for (int i = 0; i < N; i++) { p->element[i] = NULL; }
+		}
+		p->element[Tam] = x;
+		Tam++;
+	}
+	if (Tam == N){
+		p->Full = true;
+		Size++;
+		Tam = 0;
+		p->Next = new Nodo();
+		p = p->Next;
+	}
+	cout << Size<<Tam << "/" << x << "\n";
+}
+template<class T, int N>
+void SimpleListB<T, N>::insert(T x, int pos)
+{
+	if (!Head) {
+		Head = new Nodo();
+		for (int i = 0; i < N; i++) { Head->element[i] = NULL; }
+	}
+	if (pos < N) {
+		for (int i = N-1; i>pos; i--) { Head->element[i] = Head->element[i - 1]; }
+		Head->element[pos] = x;
+		Tam++;
+	}
+}
+template<class T, int N>
+bool SimpleListB<T, N>::remove(int pos, T& x)
+{
+	return false;
+}
+template<class T, int N>
+bool SimpleListB<T, N>::pop(T& x)
+{
+	return false;
+}
+template<class T, int N>
+bool SimpleListB<T, N>::pop_back(T& x)
+{
+	return false;
+}
+template<class T, int N>
+bool  SimpleListB<T, N>::get(int pos, T& x)
+{
+	return false;
+}
+template<class T, int N>
+bool SimpleListB<T, N>::get_front(T& x)
+{
+	return false;
+}
+template<class T, int N>
+bool SimpleListB<T, N>::get_back(T& x)
+{
+	return false;
+}
+template<class T, int N>
+void SimpleListB<T, N>::print()
+{
+	cout << ListName << " = [";
+	if (Head) {
+		Link p = Head;
+		while (p) {
+		int i = 0;
+		cout << "[";
+		cout << p->element[i];
+		while (i<N-1){
+			cout << ", " << p->element[i+1];
+			i++;
+		}
+		p = p->Next;
+		cout << "]";
+		}
+	}
+	cout << "]\n";
+}
+//end region
+#endif // !SimpleListB
